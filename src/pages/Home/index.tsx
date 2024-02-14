6267919682b2dd02f1c9d3f0449836573fcfd9fc
sujil <sujilnt@gmail.com>
2024-02-14 08:08:15 +0000

    basic swipe with buttons ready

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import {useState} from "react";
 import TinderCard from "react-tinder-card";
 import logo from '../../assets/logo.png';
+import {IconButton} from "@material-tailwind/react";
+
 import './styles.css';
 
 interface Person {
@@ -18,10 +20,10 @@ export default function Home() {
 
     return (
         <div className='w-full flex justify-center'>
-            <div className='w-4/12 h-screen border rounded'>
+            <div className='w-4/12 h-screen border rounded flex flex-col'>
                 <div>
                     <img
-                        className="h-20 w-full object-cover object-center"
+                        className="max-h-20 w-full object-cover object-center"
                         src={logo}
                         alt="nature image"
                     />
@@ -36,13 +38,29 @@ export default function Home() {
                             noCardLeftScreen={() => outOfFrame(person.name)}
                         >
                             <div
-                                style={{ backgroundImage: `url(${person.url})` }}
+                                style={{backgroundImage: `url(${person.url})`}}
                                 className="card"
                             >
                                 <h3>{person.name}</h3>
                             </div>
                         </TinderCard>);
                     })}
+                </div>
+                <div className="flex justify-around">
+                    <IconButton className="bg-pink-500 rounded-full size-24 inline-block" placeholder='Replay'>
+                        <i className="fas fa-reply" />
+                    </IconButton>
+                    <IconButton className="bg-pink-500 rounded-full size-24 inline-block" placeholder={'close'}>
+                        <i className="fas fa-close"/>
+                    </IconButton>
+
+                    <IconButton className="bg-pink-500 rounded-full size-24 inline-block" placeholder={'star'}>
+                        <i className="fas fa-star"/>
+                    </IconButton>
+
+                    <IconButton className="bg-pink-500 rounded-full w-20 h-20 inline-block" placeholder='favorite'>
+                        <i className="fas fa-heart"/>
+                    </IconButton>
                 </div>
             </div>
         </div>
